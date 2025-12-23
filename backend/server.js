03629b5cf241c640bdcc9b2e44084484b28b82b6
Ananniya Acharya <166976727+SlimmShadyyy@users.noreply.github.com>
2025-12-23 11:39:46 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -19,9 +19,13 @@ app.use("/api/student", studentRoutes);
 
 const PORT = process.env.PORT || 5000;
 
-mongoose.connect(process.env.MONGO_URI).then(() => {
-  app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
-
-
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.error("MongoDB connection error:", err);
+  });
