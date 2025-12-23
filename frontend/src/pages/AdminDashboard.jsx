de21459c9f39a28ab100e79e7fc576bef06a212f
Ananniya Acharya <166976727+SlimmShadyyy@users.noreply.github.com>
2025-12-23 12:10:22 +0530

    Update AdminDashboard.jsx

--- frontend/src/pages/AdminDashboard.jsx
+++ frontend/src/pages/AdminDashboard.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import axios from "axios";
+import api from "../services/api";
 
 export default function AdminDashboard() {
   const [students, setStudents] = useState([]);
