39935273057557ca955f1a53dc17ae514d606d1b
Ananniya Acharya <166976727+SlimmShadyyy@users.noreply.github.com>
2025-12-23 11:34:53 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -17,6 +17,11 @@ app.use("/api/auth", authRoutes);
 app.use("/api/admin", adminRoutes);
 app.use("/api/student", studentRoutes);
 
+const PORT = process.env.PORT || 5000;
+
 mongoose.connect(process.env.MONGO_URI).then(() => {
-  app.listen(5000, () => console.log("Server running 🚀"));
+  app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
+
+
