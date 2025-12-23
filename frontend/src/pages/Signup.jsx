f93a80b57350cef3de5a35033d4a47b75cc0d0e9
Ananniya Acharya <166976727+SlimmShadyyy@users.noreply.github.com>
2025-12-23 12:10:09 +0530

    Update Signup.jsx

--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import axios from "axios";
+import api from "../services/api";
 import { useNavigate, Link } from "react-router-dom";
 
 export default function Signup() {
