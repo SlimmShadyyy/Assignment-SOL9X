4405b8e5bc5b0d588f0c6d609c13b93c534091a7
Ananniya Acharya <166976727+SlimmShadyyy@users.noreply.github.com>
2025-12-23 12:32:09 +0530

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,8 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  console.log("API BASE URL:", import.meta.env.VITE_API_BASE_URL);
-  baseURL: import.meta.env.VITE_API_BASE_URL || "",
+  baseURL: import.meta.env.VITE_API_BASE_URL,
 });
 
 export default api;
